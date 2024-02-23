9ea1c7a3d25551c540575e4a2b4a8e3629d3922a
郑浩楠 <zhenghaonan@induschain.cn>
2024-02-23 17:53:14 +0800

    feat: 回复到以前的案例

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import MyFun from './views/MyFun.js';
 import MyClass from './views/MyClass.js';
 
 export default function App(props) {
-  const [testValue] = useState(123213)
   console.log('App组件运行了')
   useEffect(() => {
     console.log('App useEffect')
   }, [])
   return (
     <div className="App">
-      <div>react源码调试  <div>1111</div> {testValue}</div>
+      <div>react源码调试</div>
       <MyFun name='MyFun'></MyFun>
       <MyClass name='MyClass'></MyClass>
     </div>
